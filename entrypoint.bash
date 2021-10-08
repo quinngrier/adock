0e01cc85224f8d4302aace68b7b1330ec202c92b
Quinn Grier <quinn@quinngrier.com>
2021-10-08 03:55:41 -0700

    Make entrypoint.bash more robust

--- entrypoint.bash
+++ entrypoint.bash
@@ -20,6 +20,18 @@ if [[ "${1-}" != --serve ]]; then
   exec asciidoctor "$@"
 fi
 
+barf() {
+  if (($# == 0)); then
+    printf '%s\n' "$0: Unknown error." >&2
+  else
+    printf '%s' "$0: $1" >&2
+    shift
+    printf ' %s' "$@" >&2
+    echo >&2
+  fi
+  exit 1
+}
+
 info() {
   declare d
   d=$(date '+%Y-%m-%d %H:%M:%S')
@@ -60,6 +72,10 @@ shift
 
 live_port=${live_addr#*:}
 readonly live_port
+r='^[1-9][0-9]{1,4}$'
+if [[ ! "$live_port" =~ $r ]] || ((live_port > 65535)); then
+  barf "Invalid live_port: $live_port"
+fi
 
 mkdir -p /adock/out
 
@@ -77,26 +93,46 @@ readonly livereload_pid
 
 popd >/dev/null
 
+pwd=$PWD
+if [[ "$pwd" == / ]]; then
+  pwd=
+fi
+readonly pwd
+
+pwd_hex=
+for ((i = 0; i < ${#pwd}; ++i)); do
+  pwd_hex+=$(printf '\\\\x%02x' "'${pwd:i:1}")
+done
+readonly pwd_hex
+
 while :; do
 
   info "Running asciidoctor"
 
+  deps=()
+
+  # We assume that all non-absolute open() paths are relative to $PWD.
+
   strace \
     -e open \
     -o /adock/deps \
+    -xx \
     asciidoctor \
     -o /adock/out/index.tmp.html \
     "$@" \
   ;
-
-  deps=$(
+  xs=$(
     sed -n '/^open(/ p' /adock/deps | sed -n '
       s/open("//
       s/".*//
-      s|^[^/]|'$PWD'/&|
-      s|^'$PWD'/|&|p
+      s/^\\x\([^2]\|2[^f]\)/'$pwd_hex'\\x2f&/
+      /^'$pwd_hex'\\x2f/ p
     '
   )
+  for x in $xs; do
+    printf -v x "$x"
+    deps+=("$x")
+  done
 
   xs=$(
     node -e '
@@ -105,27 +141,41 @@ while :; do
       const file = "/adock/out/index.tmp.html";
       const data = fs.readFileSync(file, "utf8");
       const {document} = parseHTML(data);
+      const q = "'\''";
+      const qr = /'\''/g;
+      const qe = q + "\\" + q + q;
+      let paths = [];
       for (const [tag, attr] of [["a", "href"], ["img", "src"]]) {
         for (const node of document.getElementsByTagName(tag)) {
-          const path = node.getAttribute(attr);
+          const path = decodeURI(node.getAttribute(attr));
           if (!path.includes("://")) {
-            console.log(path);
+            paths.push(path);
           }
         }
       }
-    ' | sed 's|^|'$PWD'/|' | sort -u
+      for (const path of new Set(paths)) {
+        console.log(q + path.replace(qr, qe) + q);
+      }
+    '
   )
-
-  for x in $xs; do
+  eval xs="($xs)"
+  for x in "${xs[@]}"; do
+    x=$pwd/$x
     if [[ -f "$x" ]]; then
-      deps+=${deps:+$'\n'}$x
-      y=/adock/out/${x#$PWD/}
-      mkdir -p ${y%/*}
-      ln -f -s $x $y
+      deps+=("$x")
+      y=/adock/out/${x#"$pwd/"}
+      mkdir -p "${y%/*}"
+      ln -f -s "$x" "$y"
     fi
   done
 
-  sort -u <<<"$deps" >/adock/deps
+  for x in "${deps[@]}"; do
+    case $x in *$'\n'* | *$'\r'*)
+      barf "Strange file names are not supported."
+    esac
+  done
+
+  (IFS=$'\n'; sort -u <<<"${deps[*]}" >/adock/deps)
 
   inotifywait \
     --event "$inotifywait_events" \
