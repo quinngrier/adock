f929232352c5c9ed0ddc9bd5f950d16925ce0bcc
Quinn Grier <quinn@quinngrier.com>
2023-01-02 16:59:01 -0800

    Start adding some tests

---
+++ test/graphviz/test.bash
@@ -0,0 +1,14 @@
+#
+# The authors of this file have waived all copyright and
+# related or neighboring rights to the extent permitted by
+# law as described by the CC0 1.0 Universal Public Domain
+# Dedication. You should have received a copy of the full
+# dedication along with this file, typically as a file
+# named <CC0-1.0.txt>. If not, it may be available at
+# <https://creativecommons.org/publicdomain/zero/1.0/>.
+#
+
+set -E -e -u -o pipefail || exit $?
+trap exit ERR
+
+../../adock -r asciidoctor-diagram test.adoc
