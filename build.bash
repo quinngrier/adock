80a2bba4e712565d77b6a9d4dc832d5122a402ea
Quinn Grier <quinn@quinngrier.com>
2024-03-01 17:35:02 -0800

    Patch Lasem to use traditional LaTeX fonts

--- build.bash
+++ build.bash
@@ -13,18 +13,47 @@ set -E -e -u -o pipefail || exit $?
 trap exit ERR
 
 apk add --no-cache \
+  autoconf \
+  automake \
+  bison \
+  cairo-dev \
   chromium \
+  clang \
+  cmake \
   doas \
+  flex \
+  font-bakoma-ttf \
   gcc \
+  gdk-pixbuf-dev \
+  git \
+  glib-dev \
   gnuplot \
+  gtk-doc \
   inotify-tools \
+  intltool \
+  libtool \
+  libxml2-dev \
+  meson \
   musl-dev \
+  ninja \
   nodejs \
   npm \
+  pango-dev \
   patch \
   strace \
   wget \
 ;
+
+#-----------------------------------------------------------------------
+# Install the KaTeX fonts
+#-----------------------------------------------------------------------
+
+git clone https://github.com/KaTeX/KaTeX.git katex
+mkdir /usr/share/fonts/katex
+rmdir /usr/share/fonts/katex
+cp -R katex/fonts /usr/share/fonts/katex
+rm -r katex
+fc-cache -r -v
 
 #-----------------------------------------------------------------------
 # Install Pikchr
@@ -44,6 +73,21 @@ cp pikchr /bin
 popd >/dev/null
 popd >/dev/null
 rm -f -r pikchr
+
+#-----------------------------------------------------------------------
+# Install Lasem 0.5.1
+#-----------------------------------------------------------------------
+
+git clone https://github.com/LasemProject/lasem.git lasem
+cd lasem
+git checkout LASEM_0_5_1
+patch -p1 </patches/lasem/0.5.1.patch
+./autogen.sh
+./configure --prefix /usr PKG_CONFIG=pkg-config
+make
+make install
+cd ..
+rm -r lasem
 
 #-----------------------------------------------------------------------
 
